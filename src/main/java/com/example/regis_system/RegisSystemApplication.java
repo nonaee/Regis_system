993a96958c524720563d78b8dbbfc737f5dc509c
NONAE\goes__000 <ranate_sirinoy@cmu.ac.th>
2018-02-23 22:33:04 +0700

    1. start with login component.

---
+++ src/main/java/com/example/regis_system/RegisSystemApplication.java
@@ -0,0 +1,12 @@
+package com.example.regis_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisSystemApplication.class, args);
+	}
+}
