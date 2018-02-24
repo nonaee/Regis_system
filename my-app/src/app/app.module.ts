3f231cbe0ff9945af888bfaefb4ab20f92d65143
NONAE\goes__000 <ranate_sirinoy@cmu.ac.th>
2018-02-24 15:01:21 +0700

    make Home page (not finish)

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -4,15 +4,25 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import {RouterModule, Routes} from '@angular/router';
 
+const appRoutes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'login', component: LoginComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,RouterModule.forRoot(
+      appRoutes,
+      {enableTracing: false}
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
