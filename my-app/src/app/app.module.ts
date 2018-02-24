b62ba5ff7fb794782e83a6f1fe8e2d2748426997
NONAE\goes__000 <ranate_sirinoy@cmu.ac.th>
2018-02-24 18:24:31 +0700

    create course did't finish

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -6,17 +6,22 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import {RouterModule, Routes} from '@angular/router';
+import { CreateCourseComponent } from './create-course/create-course.component';
+
 
 const appRoutes: Routes = [
   {path: 'home', component: HomeComponent},
-  {path: 'login', component: LoginComponent}
+  {path: 'login', component: LoginComponent},
+  {path: 'create_course', component: CreateCourseComponent}
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    HomeComponent
+    HomeComponent,
+    CreateCourseComponent,
+
   ],
   imports: [
     BrowserModule,RouterModule.forRoot(
